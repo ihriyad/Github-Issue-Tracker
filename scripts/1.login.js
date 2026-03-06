f079dbdbcac77c97fb7172ae9b95e8c4e89240f6
ihriyad <ihriyad.dev@gmail.com>
2026-03-07 01:23:31 +0600

    fucntion for login

---
+++ scripts/1.login.js
@@ -0,0 +1,20 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  // console.log("btn clicked")
+  const getUserName = document.getElementById("input-username");
+  const userName = getUserName.value;
+
+  const getPassword = document.getElementById("input-password");
+  const password = getPassword.value;
+//   console.log(password)
+
+  if(userName === "admin" && password === "admin123")
+  {
+    alert("Login Success")
+    window.location.replace("home.html")
+  }
+  else
+  {
+    alert("Invalid Username or Password")
+  }
+  return;
+});
